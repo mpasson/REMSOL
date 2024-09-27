9ef7250c2f7ae4244b3c0ef65cfdd2e026c6ce4a
Marco Passoni <mpasson91@gmail.com>
2024-09-27 16:56:42 +0200

    Added draft python bindings

--- src/enums.rs
+++ src/enums.rs
@@ -1,9 +1,13 @@
+use pyo3::prelude::*;
+
+#[pyclass]
 #[derive(Copy, Clone)]
 pub enum BackEnd {
     Scattering,
     Transfer,
 }
 
+#[pyclass]
 #[derive(Copy, Clone)]
 pub enum Polarization {
     TE,
--- src/layer.rs
+++ src/layer.rs
@@ -1,10 +1,24 @@
+use pyo3::prelude::*;
+
+#[pyclass]
+#[derive(Debug, Copy, Clone)]
 pub struct Layer {
     pub n: f64,
     pub d: f64,
 }
 
+#[pymethods]
 impl Layer {
+    #[new]
     pub fn new(n: f64, d: f64) -> Layer {
         Layer { n, d }
+    }
+
+    fn __str__(&self) -> PyResult<String> {
+        self.__repr__()
+    }
+
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("Layer(n={}, d={})", self.n, self.d))
     }
 }
